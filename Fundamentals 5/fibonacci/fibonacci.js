c37ff593f405f0935f199f3b8a7c654c63d10445
Emmanuel Zhuwao <ezhuwao@live.com>
2021-04-08 21:37:49 +0200

    Solution to fibonacci

--- Fundamentals 5/fibonacci/fibonacci.js
+++ Fundamentals 5/fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+     if (n<=0){
+                return 'OOPS';
+            }
+    return n < 1? 0
+            : n <= 2? 1
+            : fibonacci(n-1) + fibonacci(n-2);
+           
 
 }
 
