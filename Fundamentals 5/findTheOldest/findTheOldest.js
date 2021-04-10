5cbf7a03c2d332a49bc6e59008fbb0b602d72c1b
Emmanuel Zhuwao <ezhuwao@live.com>
2021-04-10 15:26:29 +0200

    Solution to findtheOldest

--- Fundamentals 5/findTheOldest/findTheOldest.js
+++ Fundamentals 5/findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  const oldest = people.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return oldest[0];
+ }
+ 
 
-}
 
 module.exports = findTheOldest
 
