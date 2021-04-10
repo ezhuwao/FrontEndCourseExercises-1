9a25df91f9583f59024f2470d43df1f0293b2c8b
Emmanuel Zhuwao <ezhuwao@live.com>
2021-04-10 16:50:44 +0200

    solution to Palindromes

--- Fundamentals 5/palindromes/palindromes.js
+++ Fundamentals 5/palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+let re = /[\W_]/g;
+let lowRegStr = str.toLowerCase().replace(re, '');
+let reverseStr = lowRegStr.split('').reverse().join(''); 
+return reverseStr === lowRegStr;
 }
 
 module.exports = palindromes
